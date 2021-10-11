29be3f87ac1e96e9d5137f5be62374f83f00cc12
Nils Seitz <nils.seitz@icloud.com>
2021-10-11 20:07:17 +0200

    implemented conversion for self.block since its the only non-df class attr that needs further processing

--- psyparser.py
+++ psyparser.py
@@ -1,33 +1,47 @@
 import os
 
+from dataclasses import dataclass
 import pandas as pd
 
+
+# Simulates the structure from the example
+
+@dataclass(repr=True,eq=True)
 class Tokens:
     def __init__(self, exp, category, vpn, *data):
         def get_value(token):  # removes the "keyword"
             return token.split(':')[1].strip()
 
-        #  transforms the lines into a proper Pandas DataFrame
+        def convert_block(category):
+            # MAGIC CONSTANTS (SEE EXAMPLE TAB11)
+            gono = {'TGT': 'go', 'NTGT': 'no'}
+            emo = {'NEUTRAL': 'neu', 'HAPPY': 'hap', 'AFRAID': 'fea'}
+            # "TGT NEUTRAL, NTGT HAPPY" -> "go_neu_no_hap"
+            fst, snd = [x.strip().split(' ') for x in category.split(', ')]
+            gn0, emo0 = fst
+            gn1, emo1 = snd
+            return f'{gono[gn0]}_{emo[emo0]}_{gono[gn1]}_{emo[emo1]}'
+
+        # transforms the lines into a proper Pandas DataFrame
         def file_lines_to_df(file_lines):
             separat_lines = [line.split('\t') for line in file_lines]  # Tabs
-            col_name , *data_lines = separat_lines
-            trial = col_name[0]  # name of the index row
-            
+            col_name, *data_lines = separat_lines
+            trial, pic, rt, rs = col_name  # name of the index row
             # make the format right with col_name and set_index
-            df: pd.DataFrame = pd.DataFrame(data_lines,columns=col_name)
+            df = pd.DataFrame(data_lines, columns=col_name, dtype="string")
             df = df.set_index(trial)
-            
-            # 
-            
-
-            print(df.dtypes)
+            # change types to int64 with numerics
+            df[rt] = pd.to_numeric(df[rt])
+            df[rs] = pd.to_numeric(df[rs])
             return df
 
-
         self.experiment = get_value(exp)
-        self.block = get_value(category)
+        self.block = convert_block(get_value(category))
         self.id = get_value(vpn)
-        self.df: pd.DataFrame = file_lines_to_df(file_lines=data)
+        self.df = file_lines_to_df(file_lines=data)
+
+    def __str__(self):
+        return self.__repr__()
 
 
 
@@ -37,17 +51,18 @@ class Parser:
     def __init__(self, path_rootdir):
         for root, _, file in os.walk(path_rootdir):
             self.filepaths = [os.path.join(root, f) for f in file if
-                                         f.endswith('.txt')]
+                              f.endswith('.txt')]
 
     # for debugging purposes
     def __str__(self):
         return self.filepaths.__str__()
 
     # transformation and saving to .csv for each file
-    def run(self,savepath):
+    def run(self, path_savedir):
         for filepath in self.filepaths:
             file_lines = self.lines_filepath(filepath)
             tokens = Tokens(*file_lines)
+            print(tokens)
             break  # TODO: DEBUG BREAK POINT
 
     # reads in all lines of a file for a given path to a list
