3046d2e3eb6ca63f0ca130ade5df1b1976b29710
Nils Seitz <nils.seitz@icloud.com>
2021-10-11 19:12:25 +0200

    added savepath to main and as arg to parser.run()

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from psyparser import Parser
 
 
 def main():
-    rootpath: str = './data/'
-    parser: Parser = Parser(path_rootdir=rootpath)
-    parser.run()
+    rootpath= './data/'
+    savepath= './out/'
+
+    parser= Parser(path_rootdir=rootpath)
+    parser.run(path_savedir=savepath)
+
 
 if __name__ == '__main__':
     main()
